bd9f42cd01e9fe1cfd2bc17dc45ae53953ef7dac
Andres Ramirez <andresramirezz@hotmail.com>
2020-04-28 14:48:18 -0400

    Strings Introduction exercise

---
+++ src/com/hackerrank/Strings/Solutions.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.hackerrank.Strings;
+
+/**
+ * @author andresr
+ *
+ */
+public class Solutions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
