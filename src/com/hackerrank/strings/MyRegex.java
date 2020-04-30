be5ae277a9a1b94cbac79eacd8f74d313dbc64f4
Andres Ramirez <andresramirezz@hotmail.com>
2020-04-30 19:50:35 -0400

    Add MyRegex.java and Regex.java exercises
    Signed-off-by: Andres Ramirez <andresramirezz@hotmail.com>

---
+++ src/com/hackerrank/strings/MyRegex.java
@@ -0,0 +1,13 @@
+package com.hackerrank.strings;
+
+/**
+ * @author andresr
+ *
+ */
+
+public class MyRegex {
+	
+	String str = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25"
+			+ "[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+	String pattern = str;
+}
---
+++ src/com/hackerrank/strings/Regex.java
@@ -0,0 +1,26 @@
+package com.hackerrank.strings;
+
+import java.util.Scanner;
+
+/**
+ * @author andresr
+ * Problem: Write a class called MyRegex which will contain a string pattern. 
+ * You need to write a regular expression and assign it to the pattern such 
+ * that it can be used to validate an IP address. Use the following definition 
+ * of an IP address: IP address is a string in the form "A.B.C.D", where the 
+ * value of A, B, C, and D may range from 0 to 255. Leading zeros are allowed. 
+ * The length of A, B, C, or D can't be greater than 3.
+
+ */
+public class Regex {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches( new MyRegex().pattern));
+		}
+		in.close();
+	}
+}
